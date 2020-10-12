e0f274563f081024fac22333afd1b2687a151fbb
agunghimura11 <agung.arisudana@gmail.com>
2020-10-12 17:31:38 +0800

    init body parser & handle form data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
 import morgan from 'morgan'
+import bodyParser from 'body-parser';
 
 const __dirname = path.resolve()
 
@@ -16,12 +17,23 @@ app.use(morgan('combined'))
 
 app.use('/assets', express.static(__dirname + 'assets'))
 
+app.use(bodyParser.urlencoded())
+
 app.get('/', (req,res,next) => {
     res.send({success:true})
 })
 
 app.get('/product', (req,res,next) => {
     res.render('product')
+})
+
+app.get('/add-product', (req,res,next) => {
+    res.send(req.query)
+})
+
+app.post('/add-product', (req,res,next)=> {
+ console.log('Request', req.body)
+ res.send(req.body)
 })
 
 app.use((err,req,res,next) => {

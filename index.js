cf61c3b9468e80a41616f9f2cb62a025f3a6022a
agunghimura11 <agung.arisudana@gmail.com>
2020-10-12 17:01:58 +0800

    init layout & serve static files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
+import morgan from 'morgan'
 
 const __dirname = path.resolve()
 
@@ -10,12 +11,21 @@ app.set('views', __dirname + '/layouts')
 app.set('view engine', 'html')
 app.engine('html', hbs.__express)
 
+
+app.use(morgan('combined'))
+
+app.use('/assets', express.static(__dirname + 'assets'))
+
 app.get('/', (req,res,next) => {
     res.send({success:true})
 })
 
-app.get('/', (req,res,next) => {
-    res.send({success: true})
+app.get('/product', (req,res,next) => {
+    res.render('product')
+})
+
+app.use((err,req,res,next) => {
+    res.send(err.message)
 })
 
 app.listen(8000, () => {
